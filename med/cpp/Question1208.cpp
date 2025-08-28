2aa2be5c418a2c447d8f81c78d5f9868a68817f7
soongfs <soongfs@proton.me>
2025-08-28 15:53:33 +0800

    update

---
+++ med/cpp/Question1208.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) {
+        int ans=0,cost=0,left=0,n=s.length();
+        for (int right=0;right<n;right++) {
+            cost+=abs(s[right]-t[right]);
+            while (cost>maxCost) {
+                cost-=abs(s[left]-t[left]);
+                left++;
+            }
+            ans=max(ans,right-left+1);
+        }
+        return ans;
+    }
+};
