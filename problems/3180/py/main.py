f16471d73fb3619a231023113f1c86d13c31cb6d
soongfs <soongfs04@gmail.com>
2026-01-15 15:34:21 +0800

    backup: 2026-01-15 15:34:21

---
+++ problems/3180/py/main.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def maxTotalReward(self, rewardValues: List[int]) -> int:
+        f = 1
+        for v in sorted(set(rewardValues)):
+            f |= (f & ((1 << v) - 1)) << v
+        return f.bit_length() - 1
